c1a44792232e855bbd6e3af7570fc39f4e47e4e8
Marce-998 <marceloezequieloviedo@gmail.com>
2024-10-09 21:06:15 -0300

    se agrega un cont al .sh para que pare en el ciclo 100 y no sea infinito

--- SEGURIDAD INFORMATICA/findError.sh
+++ SEGURIDAD INFORMATICA/findError.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+count=0 
 while true; do
-cat input.txt | radamsa | tee last_input.txt | ./insert
-test $? -gt 127 && break
+    cat input.txt | radamsa | tee last_input.txt | ./insert
+    test $? -gt 127 && break
+    count=$((count + 1))  # Incrementamos el contador
+    if [ "$count" -ge 100 ]; then  
+        echo "Se Alcanzó el límite de 100 iteraciones."
+        break
+    fi
 done
